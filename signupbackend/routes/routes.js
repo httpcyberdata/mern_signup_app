9e9abb2ab4c7e978284d14b2bf47407772cf1d44
httpcyberdata <shieldedsafety@gmail.com>
2023-04-30 08:46:10 -0400

    chore: created /signup route, added mongoDB configuration

---
+++ signupbackend/routes/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const signUpTemplateCopy = require('../models/SignUpModels');
+
+router.post('/signup', (req, res) => {
+    const signedUpUser = new signUpTemplateCopy({
+        fullName: req.body.fullName,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    signedUpUser.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err);
+        })
+});
+
+router.get('/signin', (req, res) => {
+    
+})
+
+
+module.exports = router;
--- signupbackend/server.js
+++ signupbackend/server.js
@@ -1,4 +1,14 @@
 const express = require('express'); 
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const routesUrls = require('./routes/routes');
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_ACCESS).then(() => {
+    console.log('Connected to Database')
+});
+
+app.use('/app', routesUrls);
 
 app.listen(4000, () => console.log("Server is up and running"));
