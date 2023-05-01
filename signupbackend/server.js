b6e6a5eec42e0649c9c4a18659faa6ac050c0f9e
httpcyberdata <shieldedsafety@gmail.com>
2023-05-01 09:41:22 -0400

    created sign up form via class component

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -3,11 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const routesUrls = require('./routes/routes');
+const cors = require('cors');
+
 dotenv.config();
 
 mongoose.connect(process.env.DATABASE_ACCESS).then(() => {
     console.log('Connected to Database')
 });
+
+app.use(express.json());
+
+app.use(cors());
 
 app.use('/app', routesUrls);
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      fullName: '',
+      username: '',
+      email: '',
+      password: ''
+    }
+  }
+
+  changeFullName(event) {
+    this.setState({
+      fullName: event.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="container" style={{ marginTop: '120px'}}>
+        <div className="form-div">
+            <form>
+              <input 
+                type="text"
+                placeholder='Full Name' 
+                onChange={this.changeFullName} 
+                value={this.state.fullName}
+                className='form-control form-group'
+              />
+              <input 
+                type="text"
+                placeholder='Username'
+                onChange={this.changeUsername}
+                value={this.state.username} 
+                className="form-control form-group"
+              />
+              
+              <input
+                placeholder="E-mail"
+                onChange={this.changeEmail}
+                value={this.state.email}
+                className='form-control form-group'
+              />
+              
+              <input 
+                  type="password"
+                  placeholder='Password'
+                  onChange={this.changePassword}
+                  value={this.state.password}
+                  className='form-control form-group'
+              />
+              <input type="submit" className="btn btn-danger" />
+            </form>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
